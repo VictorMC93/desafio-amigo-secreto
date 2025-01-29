c0d16e2e5e817cc0c7ee3cf0e814012561815d4f
VictorMC93 <v-moraga@hotmail.com>
2025-01-29 19:29:10 -0300

    Actualización app.js (array, funcion de agregado)

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigosLista=[]; //Array de amigos ingresados
+function agregarAmigo() { //Funcion para agregar nombres a array
+    let amigoAgregado=document.getElementById('amigo').value; //añade el contenido escrito por el usuario a variable
+    if (amigoAgregado==''){ //si el usuario no escribe nada
+        alert('Por favor, inserte un nombre.');
+    }else{ //agrega nombre a array y limpia campo de entrada
+        amigosLista.push(amigoAgregado);
+        document.getElementById('amigo').value='';
+        console.log(amigosLista);
+        generarListaAmigos();
+    }
+    return;
+}
+function generarListaAmigos(){
+    let listaAmigos = document.getElementById("listaAmigos");
+    for (i = 0; i <= amigosLista.length; i++) {
+        let lista = document.createElement("lista");
+        lista.innerHTML=amigosLista[i];
+        listaAmigos.appendChild(lista); 
+    }
+    return;
+}
+
+function sortearAmigo() {
+    
+}
