b4f9af53521c5199eb79eac3a52eb1973d7b85d1
VictorMC93 <v-moraga@hotmail.com>
2025-01-29 22:53:34 -0300

    Actualización app.js agregado de documentación y limpieza de console.log

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let amigosLista=[]; //Array de amigos ingresados
-let resultadoSorteo;
+let resultadoSorteo; //Se declara para su uso en las funciones generarListaAmigos() y sortearAmigo()
 function agregarAmigo() { //Funcion para agregar nombres a array
     let amigoAgregado=document.getElementById('amigo').value; //añade el contenido escrito por el usuario a variable
     if (amigoAgregado==''){ //si el usuario no escribe nada
@@ -8,7 +7,6 @@ function agregarAmigo() { //Funcion para agregar nombres a array
     }else{ //agrega nombre a array y limpia campo de entrada
         amigosLista.push(amigoAgregado);
         document.getElementById('amigo').value='';
-        console.log(amigosLista);
         generarListaAmigos();
     }
     return;
@@ -16,23 +14,23 @@ function agregarAmigo() { //Funcion para agregar nombres a array
 function generarListaAmigos(){
     let listaAmigos = document.getElementById("listaAmigos"); //Obtener la lista HTML y asignar variable
     listaAmigos.innerHTML = ""; //Vaciar la lista cada vez que se agrega un nombre
-    for (i = 0; i < amigosLista.length; ++i) { 
+    for (i = 0; i < amigosLista.length; ++i) { //For para enviar el nombre escrito a lista HTML
         let li = document.createElement('li'); //crear elemento de lista
         li.innerText = amigosLista[i]; //asignar nombre de lista equivalente a indice i
-        listaAmigos.appendChild(li);  //
+        listaAmigos.appendChild(li);  // Ingresa el nombre en la lista
     }
-    resultadoSorteo.innerHTML='';
+    resultadoSorteo.innerHTML=''; // Borrar texto de nombre seleccionado al momento de agregar uno nuevo
     return;
 }
 
 function sortearAmigo() {
-    if (amigosLista.length==0) {
+    if (amigosLista.length==0) { //if para caso de que se presione boton sin nombres en el array
         alert('Debe agregar al menos un nombre');
     }else{
-        let numeroAmigos=amigosLista.length;
-        let seleccionAmigo= Math.floor(Math.random()*numeroAmigos);
-        console.log(seleccionAmigo);
-        resultadoSorteo= document.getElementById('resultado');
-        resultadoSorteo.innerHTML=amigosLista[seleccionAmigo];
+        let numeroAmigos=amigosLista.length; //Toma el total de nombres en el array
+        let seleccionAmigo= Math.floor(Math.random()*numeroAmigos); // Elige un numero entre 0 y la variable anterior
+        resultadoSorteo= document.getElementById('resultado'); //Obtener elemento que mostrara el resultado mediante su id
+        resultadoSorteo.innerHTML=amigosLista[seleccionAmigo]; //Ingresa el nombre al elemento HTML y aparece en la pantalla
     }
+    return;
 }
